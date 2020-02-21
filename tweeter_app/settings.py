6f1c29aef89e538f7b5581889ae8ea8660de7d01
carolakey <carol.key42@gmail.com>
2020-02-20 20:42:04 -0600

    Configuring for SendGrid

--- tweeter_app/settings.py
+++ tweeter_app/settings.py
@@ -120,10 +120,19 @@ USE_L10N = True
 
 USE_TZ = True
 
+BOOTSTRAP4 = { 'include_jquery': True }
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-BOOTSTRAP4 = { 'include_jquery': True }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
